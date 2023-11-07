134cb1375925251bc2f94f1fa221b014f545a86e
belhadj haitam <steankir.haitam@gmail.com>
2023-11-07 15:31:34 +0100

    walo

--- exec/pipes.c
+++ exec/pipes.c
@@ -6,11 +6,66 @@
 /*   By: hbelhadj <hbelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 13:18:03 by hbelhadj          #+#    #+#             */
-/*   Updated: 2023/11/07 14:13:02 by hbelhadj         ###   ########.fr       */
+/*   Updated: 2023/11/07 15:25:12 by hbelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+void child(int fd[2], t_data_cmd *cmd, int *save, int i)
+{
+            // signal(SIGQUIT, SIG_DFL);
+	        // signal(SIGINT, SIG_DFL);
+            char    *path;
+
+            if (i == 0 && fd[1] != -1)
+                dup2(fd[1], STDOUT_FILENO);
+            else if (i == cmd->cmd_size - 1 && *save != -1)
+                dup2(*save, STDIN_FILENO);
+            else if (i < cmd->cmd_size - 1 && i > 0 && *save != -1 && fd[1] != -1)
+            {
+                dup2(*save, STDIN_FILENO);
+                dup2(fd[1], STDOUT_FILENO);
+            }
+            if (*save != -1)
+                close(*save);
+            if (fd[0] != -1)
+                close(fd[0]);
+            if (fd[1] != -1)
+                close(fd[1]);
+            path = get_path(cmd->cmds[i].cmd_args[0], cmd);
+            if (path)
+            {
+                execve(path, cmd->cmds[i].cmd_args, NULL);
+                ft_putstr_fd("Error\n", 2);
+                exit(1);
+            }
+            exit(1);
+}
+
+void parent(int pid, int *save, int fd[2])
+{
+        if (pid > 0)
+        {
+            close(*save);
+            *save = fd[0];
+            close(fd[1]);
+            fd[0] = -1;
+            fd[1] = -1;
+        }
+}
+
+void _casse(int *save, int pid)
+{
+    close(*save);
+    char raw_status[4];
+    waitpid(pid, (int *)raw_status, 0);
+    if (raw_status[0])
+        s_help.exit_status = raw_status[0] + 128;
+    else
+        s_help.exit_status = raw_status[1];
+    while (waitpid(-1, NULL, 0) != -1)
+            ;
+}
 
 void execute_compund(t_data_cmd *cmd)
 {
@@ -18,13 +73,11 @@ void execute_compund(t_data_cmd *cmd)
     int     save;
     int     i;
     int     fd[2];
-    char    *path;
 
     fd[0] = -1;
     fd[1] = -1;
     save = -1;
     i = -1;
-    
     while (++i < cmd->cmd_size) 
     {
         if (i != cmd->cmd_size - 1)
@@ -35,55 +88,10 @@ void execute_compund(t_data_cmd *cmd)
                 exit(127);
             }
         }
-        
         pid = fork();
         if (pid == 0)
-        { 
-            // signal(SIGQUIT, SIG_DFL);
-	        // signal(SIGINT, SIG_DFL);
-            if (i == 0 && fd[1] != -1)
-                dup2(fd[1], STDOUT_FILENO);
-            else if (i == cmd->cmd_size - 1 && save != -1)
-                dup2(save, STDIN_FILENO);
-            else if (i < cmd->cmd_size - 1 && i > 0 && save != -1 && fd[1] != -1)
-            {
-                dup2(save, STDIN_FILENO);
-                dup2(fd[1], STDOUT_FILENO);
-            }
-            if (save != -1)
-                close(save);
-            if (fd[0] != -1)
-                close(fd[0]);
-            if (fd[1] != -1)
-                close(fd[1]);
-                
-            path = get_path(cmd->cmds[i].cmd_args[0], cmd);
-            if (path)
-            {
-                execve(path, cmd->cmds[i].cmd_args, NULL);
-                ft_putstr_fd("Error\n", 2);
-                exit(1);
-            }
-            exit(1);
-        }
-
-        if (pid > 0)
-        {
-            close(save);
-            save = fd[0];
-            close(fd[1]);
-            fd[0] = -1;
-            fd[1] = -1;
-        }
+            child(fd, cmd, &save, i);
+        parent(pid, &save, fd);
     }
-    close(save);
-    char raw_status[4];
-    waitpid(pid, (int *)raw_status, 0);
-    printf("%d %d %d %d\n", raw_status[0], raw_status[1], raw_status[2], raw_status[3]);
-    if (raw_status[0])
-        s_help.exit_status = raw_status[0] + 128;
-    else
-        s_help.exit_status = raw_status[1];
-    while (waitpid(-1, NULL, 0) != -1)
-        ;
-}
+    _casse(&save, pid);
+i}
